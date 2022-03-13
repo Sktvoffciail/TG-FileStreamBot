326561ae5b1e08cd6e631f771548e71b1d7d3663
Sktvoffciail <88933006+Sktvoffciail@users.noreply.github.com>
2022-03-13 13:41:56 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
 
 
-@StreamBot.on_message(filters.command(["start"]))
+@StreamBot.on_message(filters.command(["start", "Start"]))
 async def start(_, m: Message):
     await m.reply(
         f'Hello send or send any video, document, file, audio we will upload on our website and give you the link\n Powered By Jdisk.in'
